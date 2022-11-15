07cfe3a0364df504503a42cd1f3a732ba27b4d7d
Nam1612 <96403899+Nam1612@users.noreply.github.com>
2022-11-15 13:52:03 +0700

    Add files via upload

---
+++ src/main/java/com/example/btqt1/OptionMenu.java
@@ -0,0 +1,51 @@
+package com.example.btqt1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Message;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class OptionMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater im = getMenuInflater();
+        im.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_inf:
+               AlertDialog.Builder info = new AlertDialog.Builder(this);
+               info.setMessage("Bai Tap Qua Trinh 01 nhom \n NHOM 16 \n Link github: https://github.com/LyMinh85/BT-QT-1");
+               info.setNegativeButton("No", null);
+                info.setPositiveButton("Yes", null);
+                info.show();
+
+         }
+
+
+
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
+
+}
+
